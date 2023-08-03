37db1c469536157ac591bd7d0711407ca96a1b33
happyOBO <44173619+happyOBO@users.noreply.github.com>
2023-08-03 22:09:24 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 3.76 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.cpp
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -16,14 +16,47 @@ string solution(vector<int> numbers) {
 	sort(v.begin(), v.end(),
 		[](string a, string b)
 		{
-			return a + b > b + a;
+			int min_len = min(a.size(), b.size());
+			int max_len = max(a.size(), b.size());
+
+			if (min_len == max_len)
+			{
+				return a > b;
+			}
+
+			else
+			{
+				for (int i = 0; i < min_len; i++)
+				{
+					if (a[i] != b[i])
+						return a[i] > b[i];
+				}
+
+				// 123 , 12345 와 같이 숫자가 앞에서부터 포함 관계인 경우
+				if (a.size() < b.size())
+				{
+					for (int i = min_len; i < max_len; i++)
+					{
+						if (a[0] != b[i])
+							return a[0] > b[i];
+					}
+				}
+				else
+				{
+					for (int i = min_len; i < max_len; i++)
+					{
+						if (a[i] != b[0])
+							return a[i] > b[0];
+					}
+				}
+			}
+
+			return true;
 		});
 
 	for(string& num : v)
 	{
 		answer += num;
 	}
-	if (answer[0] == '0')
-		return "0";
 	return answer;
 }
