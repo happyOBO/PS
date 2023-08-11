1f512e82ef31a08b553c0d6c271b6547211ca8c4
happyOBO <44173619+happyOBO@users.noreply.github.com>
2023-08-11 21:10:58 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.13 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include <climits>
+#include <sstream>
+
+using namespace std;
+
+
+int bfs(int start, int n, vector<vector<int>>& wires_graph, pair<int, int> banned)
+{
+	vector<bool> visited = vector<bool>(n, false);
+	queue<int> q;
+	visited[start] = true;
+	q.push(start);
+	int curr, counter = 1;
+	while (!q.empty())
+	{
+		curr = q.front(); q.pop();
+		for (int next : wires_graph[curr])
+		{
+			if (!visited[next] &&
+				// 자른 구간으로 넘어가 버리면 안된다.
+				! ( banned.first == curr && banned.second == next ) &&
+				! ( banned.first == next && banned.second == curr ))
+			{
+				visited[next] = true;
+				counter++;
+				q.push(next);
+			}
+		} 
+	}
+	return counter;
+}
+
+
+int solution(int n, vector<vector<int>> wires) {
+
+	vector<vector<int>> wires_graph = vector<vector<int>>(n);
+
+	for (int i = 0; i < wires.size(); i++)
+	{
+		wires_graph[wires[i][0] - 1].push_back(wires[i][1] - 1);
+		wires_graph[wires[i][1] - 1].push_back(wires[i][0] - 1);
+	}
+
+	int wire_count, min_diff = INT_MAX;
+
+	// bfs
+	for (int i = 0; i < wires.size(); i++)
+	{
+		// wires[i][0] --> wires[i][1] 로 가는게 끊겨 버렸으니
+		// wires[i][1] 에서 start
+		wire_count = bfs(wires[i][1] - 1, n, wires_graph, { wires[i][0] - 1 , wires[i][1] - 1 });
+		min_diff = min(min_diff, abs(n - wire_count - wire_count ));
+	}
+	return min_diff;
+}
