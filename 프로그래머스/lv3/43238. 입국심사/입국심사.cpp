21cb5dfa31ec0d13959c29126088b39d8217bd8e
happyOBO <44173619+happyOBO@users.noreply.github.com>
2023-08-23 21:38:41 +0900

    [level 3] Title: 입국심사, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.cpp
@@ -1,44 +1,47 @@
-#include <bits/stdc++.h>
-#include <climits>
-#include <sstream>
+#include <string>
+#include <vector>
+#include <limits.h>
 
 using namespace std;
 
-bool CheckJudgeAll(int n, vector<int>& times, long long total_time)
-{
-	long long count = 0;
-	for (auto it = times.begin(); it != times.end(); it++)
-		count += (total_time / static_cast<long long>(*it));
-	return n <= count;
+
+long long solution(int n, vector<int> times);
+bool canJudgeN(long long targetTimes, int n, vector<int>& times);
+
+
+long long solution(int n, vector<int> times) {
+    int minTime = INT_MAX;
+    int maxTime = INT_MIN;
+    long long answer = LONG_MAX;
+    for (auto it = times.begin(); it != times.end(); it++)
+    {
+        minTime = min(minTime, (*it));
+        maxTime = max(maxTime, (*it));
+    }
+
+    long long minTotalTime = static_cast<long long>(minTime) * static_cast<long long>(n / times.size());
+    long long maxTotalTime = static_cast<long long>(maxTime) * max(static_cast<int>(n / times.size()), 1);
+    long long midTotalTime = (minTotalTime + maxTotalTime) / 2;
+
+    while (minTotalTime <= maxTotalTime)
+    {
+        if (canJudgeN(midTotalTime, n, times))
+        {
+            maxTotalTime = midTotalTime - 1;
+            answer = min(answer, midTotalTime);
+        }
+        else
+            minTotalTime = midTotalTime + 1;
+        midTotalTime = (minTotalTime + maxTotalTime) / 2;
+
+    }
+    return answer;
 }
 
-long long solution(int n, vector<int> times)
+bool canJudgeN(long long targetTimes, int n, vector<int>& times)
 {
-	int min_time = INT_MAX, max_time = INT_MIN;
-	for (auto time : times)
-	{
-		min_time = min(time, min_time);
-		max_time = max(time, max_time);
-	}
-
-	long long max_total_time = static_cast<long long>(max_time) * static_cast<long long>(n / times.size());
-	long long min_total_time = static_cast<long long>(min_time) * max(static_cast<int>(n / times.size()), 1);
-	long long mid_total_time = ( max_total_time + min_total_time ) / 2;
-	long long answer = max_total_time;
-
-	while (min_total_time <= max_total_time)
-	{
-		if (CheckJudgeAll(n, times, mid_total_time))
-		{
-			answer = min(mid_total_time, answer);
-			max_total_time = mid_total_time - 1;
-		}
-		else
-		{
-			min_total_time = mid_total_time + 1;
-		}
-		mid_total_time = (max_total_time + min_total_time) / 2;
-	}
-
-	return answer;
+    long long count = 0;
+    for (auto it = times.begin(); it != times.end(); it++)
+        count += (targetTimes / static_cast<long long>(*it));
+    return n <= count;
 }
