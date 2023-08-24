115a4c836e670c22453292bfaee357b28de427b9
happyOBO <44173619+happyOBO@users.noreply.github.com>
2023-08-24 22:26:40 +0900

    [level 4] Title: 징검다리, Time: 4.77 ms, Memory: 4.66 MB -BaekjoonHub

--- 프로그래머스/lv4/43236. 징검다리/징검다리.cpp
+++ 프로그래머스/lv4/43236. 징검다리/징검다리.cpp
@@ -1,53 +1,51 @@
-#include <bits/stdc++.h>
-#include <climits>
-#include <sstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int CheckDeleteRocks(int distance, int min_dist, vector<int>& rocks)
+bool CheckDeleteRocks(int distance, int min_dist, vector<int>& rocks)
 {
-	int deleted_rocks = 0;
-	int curr_pos = 0;
-	int curr_dist = 0;
-	for (int& rock : rocks)
-	{
-		if (min_dist <= rock - curr_pos)
-		{
-			curr_pos = rock;
-		}
-		else
-		{
-			deleted_rocks++;
-		}
-	}
-	if (distance - curr_pos < min_dist)
-	{
-		return deleted_rocks + 1;
-	}
-	return deleted_rocks;
+    int deleted_rocks = 0;
+    int curr_pos = 0;
+    int curr_dist = 0;
+    for(int& rock : rocks)
+    {
+        if(min_dist <= rock - curr_pos)
+        {
+            curr_pos = rock;
+        }
+        else
+        {
+            deleted_rocks++;
+        }   
+    }
+    if(min_dist <= distance - curr_pos)
+    {
+        return deleted_rocks + 1;
+    }
+    return deleted_rocks;
 }
 
 int solution(int distance, vector<int> rocks, int n)
 {
-	int max_distance = distance;
-	int min_distance = 0;
-	int mid = (max_distance + min_distance) / 2;
-	int answer = min_distance;
+    int max_distance = distance;
+    int min_distance = 0;
+    int mid = (max_distance + min_distance ) / 2;
+    int answer = min_distance;
+    
+    while(min_distance <= max_distance)
+    {
+        if(CheckDeleteRocks(distance, mid, rocks) <= n)
+        {
+            max_distance = mid - 1;
+            answer = max(answer, mid);
+        }
+        else
+        {
+            min_distance = mid + 1;
+        }
+        mid = (max_distance + min_distance) / 2;
+    }
 
-	sort(rocks.begin(), rocks.end());
-	while (min_distance <= max_distance)
-	{
-		if (CheckDeleteRocks(distance, mid, rocks) <= n)
-		{
-			min_distance = mid + 1;
-			answer = max(answer, mid);
-		}
-		else
-		{
-			max_distance = mid - 1;
-		}
-		mid = (max_distance + min_distance) / 2;
-	}
-
-	return answer;
+    return answer;
 }
